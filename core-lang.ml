4b052df27acba94f31827ed6654a1affbf14d108
Roshan Sharma <rossharma1@gmail.com>
2019-11-16 15:27:42 -0600

    getconstraints function

--- core-lang.ml
+++ core-lang.ml
@@ -1,37 +1,36 @@
-type exp = Int of int | Add of exp * exp | Sub of exp * exp
-         | Bool of bool | And of exp * exp | Or of exp * exp
+type ident = string 
+
+type exp = Var of ident | Int of int | Add of exp * exp 
+         | Sub of exp * exp| Mult of exp * exp 
+         | Bool of bool | Not of bool | And of exp * exp | Or of exp * exp
          | Eq of exp * exp | If of exp * exp * exp
 
-type cmd = Assign of ident * exp | Seq of cmd * cmd | Skip
-         | IfC of exp * cmd * cmd | While of exp * cmd
-         | Call of ident * ident * exp list | Return of exp
+let next = ref 0
+type tident = int
+let fresh_tident (u : unit) : tident = next := !next + 1; !next
 
 type typ = Tint | Tbool
+type constraints = (typ * typ) list
+type context = ident -> typ option
 
 
-let rec type_of (e : exp) : typ option = 
-      match e with 
-          | Int n -> Some(Tint)
-          | Add (e1,e2) -> (match type_of e1, type_of e2 with
-                            | Some (Tint), Some (Tint) -> Some (Tint)
-                            | _, _ -> None)
-          | Sub (e1,e2) -> (match type_of e1, type_of e2 with
-                            | Some (Tint), Some (Tint) -> Some (Tint)
-                            | _, _ -> None)
-          | Bool b -> Some (Tbool)
-          | And (e1,e2) -> (match type_of e1, type_of e2 with
-                            | Some (Tbool), Some (Tbool) -> Some (Tbool)
-                            | _, _ -> None)
-          | Or (e1,e2) -> (match type_of e1, type_of e2 with
-                            | Some (Tbool), Some (Tbool) -> Some (Tbool)
-                            | _, _ -> None)
-          | Eq (e1,e2) ->  (match type_of e1, type_of e2 with
-                             | Some (Tbool), Some (Tbool) -> Some (Tbool)
-                             | Some (Tint), Some (Tint) -> Some (Tbool) 
-                             | _, _ -> None)
-          | If (e, e1, e2) -> match type_of e with 
-                                | Some (Tbool) -> (match type_of e1, type_of e2 with
-                                      | Some (Tbool), Some (Tbool) -> Some (Tbool)
-                                      | Some (Tint), Some (Tint) -> Some (Tint) 
-                                      | _, _ -> None)
-                                | _ ->None
+let rec get_constraints (gamma : context) (e : exp) : (typ * constraints) option =
+ ( match e with
+  | Var x -> (match gamma x with Some t -> Some (t, []) | None -> None)
+  | Int i -> Some (Tint, [])
+  | Bool b -> Some (Tbool, [])
+  | Eq (e1, e2) -> (match get_constraints gamma e1, get_constraints gamma e2 with
+                     | Some (t1, c1), Some (t2, c2) ->  Some (Tbool, (t1, t2) :: c1 @ c2)
+                     | _, _ -> None)
+  | And( e1, e2) -> (match get_constraints gamma e1, get_constraints gamma e2 with
+                    | Some (t1, c1), Some (t2, c2) ->  Some (Tbool, (t1, t2) :: c1 @ c2)
+                    | _, _ -> None)
+  | Or( e1, e2) -> (match get_constraints gamma e1, get_constraints gamma e2 with
+                    | Some (t1, c1), Some (t2, c2) ->  Some (Tbool, (t1, t2) :: c1 @ c2)
+                    | _, _ -> None)
+  | If (e, e1, e2) -> (match get_constraints gamma e, get_constraints gamma e1, get_constraints gamma e2 with
+                     | Some (t, c), Some (t1, c1), Some (t2, c2) -> Some (t1, (t, Tbool) :: (t1, t2) :: c @ c1 @ c2)
+                     | _, _, _ -> None)
+  | Not b -> Some (Tbool, [])
+
+  )
