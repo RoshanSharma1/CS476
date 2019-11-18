102584e67c1e08eeae776688b590216ec7c646e9
remajin <noumanahmad655@gmail.com>
2019-11-18 15:56:33 -0600

    Added the cases for Tuple

--- core-lang.ml
+++ core-lang.ml
@@ -4,12 +4,13 @@ type exp = Var of ident | Int of int | Add of exp * exp
          | Sub of exp * exp| Mult of exp * exp 
          | Bool of bool | Not of exp | And of exp * exp | Or of exp * exp
          | Eq of exp * exp | If of exp * exp * exp | Array of ident * exp
+         | Tuple of exp * exp | Fst of exp | Snd of exp
 
 let next = ref 0
 type tident = int
 let fresh_tident (u : unit) : tident = next := !next + 1; !next
 
-type typ = Tint | Tbool | TArray of typ | Tvar of tident
+type typ = Tint | Tbool | TArray of typ | Tvar of tident | Ttuple of typ * typ
 type constraints = (typ * typ) list
 type context = ident -> typ option
 
@@ -48,5 +49,24 @@ let rec get_constraints (gamma : context) (e : exp) : (typ * constraints) option
                                     let t = fresh_tident () in
                                     Some (Tvar t,(t1, (TArray (Tvar t))) :: (t2, Tint) :: c)
                     | _, _ -> None)
+  | Tuple (e1, e2) ->
+     (match get_constraints gamma e1, get_constraints gamma e2 with
+      | Some (t1, c1), Some (t2, c2) ->
+         Some (Ttuple (t1, t2), c1 @ c2)
+      | _, _ -> None)
+  | Fst e ->
+     (match get_constraints gamma e with
+      | Some (t, c) ->
+         let t1 = fresh_tident () in
+         let t2 = fresh_tident () in
+         Some (Tvar t1, (t, Ttuple (Tvar t1, Tvar t2)) :: c)
+      | None -> None)
+  | Snd e ->
+     (match get_constraints gamma e with
+      | Some (t, c) ->
+         let t1 = fresh_tident () in
+         let t2 = fresh_tident () in
+         Some (Tvar t2, (t, Ttuple (Tvar t1, Tvar t2)) :: c)
+      | None -> None)
 
   )
