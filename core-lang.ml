dbfa58abe4d01d2f33b2382dbf4f5a89dab42ef6
Roshan Sharma <rossharma1@gmail.com>
2019-11-25 12:38:25 -0600

    Type inference for command

--- core-lang.ml
+++ core-lang.ml
@@ -8,11 +8,15 @@ type exp = Var of ident | Int of int | Add of exp * exp
          | Eq of exp * exp | If of exp * exp * exp | Array of ident * exp
          | Tuple of exp * exp | Fst of exp | Snd of exp
 
+ type cmd = Assign of ident * exp | Seq of cmd * cmd | Skip
+         | IfC of exp * cmd * cmd | While of exp * cmd
+         | Call of ident * ident * exp list | Return of exp
+
 let next = ref 0
 type tident = int
 let fresh_tident (u : unit) : tident = next := !next + 1; !next
 
-type typ = Tint | Tbool | TArray of typ | Tvar of tident | Ttuple of typ * typ
+type typ = Tint | Tbool | TArray of typ | Tvar of tident | Ttuple of typ * typ | Tfun of typ * typ list 
 type constraints = (typ * typ) list
 type context = ident -> typ option
 
@@ -73,11 +77,54 @@ let rec get_constraints (gamma : context) (e : exp) : (typ * constraints) option
 
   )
 
+  let rec get_constraints_exps (gamma : context) (es : exp list) : (typ * constraints) list option =
+   match es with
+   | [] -> Some []
+   | e :: rest -> (match get_constraints gamma e, get_constraints_exps gamma rest with
+                   | Some con1, Some cons_rest -> Some (con1 :: cons_rest)
+                   | _, _ -> None)
+
+   let types_of_cons_params (params : (typ * constraints) list) : typ list =
+   List.map fst params
+
+   let constraints_of_cons_params (params : (typ * constraints) list) : constraints =
+      List.flatten( List.map snd params )
+   
+
+   let rec make_constrainsts (param1 : typ list) (param2: typ list) : (typ * typ) list =
+      (match param1, param2 with      
+       |t1::rest1, t2 :: rest2 -> ((t1,t2):: make_constrainsts rest1 rest2 )
+       | _, _ -> [] )
+
+  let rec get_constraints_c (gamma : context) (c : cmd) : (constraints) option = 
+   (match c with 
+   | Assign(x,e) -> (match gamma x, get_constraints gamma e with 
+                     | Some t1, Some(t2, c2) -> Some((t1,t2) :: c2)
+                     | _, _ -> None)
+   | Seq(c1,c2) -> ( match get_constraints_c gamma c1, get_constraints_c gamma c2 with 
+                    | Some cons1, Some cons2 -> Some ( cons1 @ cons2)
+                    | _, _ -> None)
+   | Skip -> Some ([])
+   | IfC (e, c1, c2) -> (match get_constraints gamma e, get_constraints_c gamma c1, get_constraints_c gamma c2 with
+                        |Some (t1, con1), Some cons1, Some cons2 -> Some ((t1, Tbool) :: con1 @ cons1 @ cons1)
+                        | _,_,_ -> None)
+   | While(e, cmd) -> (match get_constraints gamma e, get_constraints_c gamma cmd with
+                       |Some (t1, con1), Some cons1 -> Some ((t1, Tbool) :: con1 @ cons1)
+                       | _,_ -> None)
+   | Call (x,f,args) -> (match gamma x, gamma f, get_constraints_exps gamma args with
+                        | Some t, Some Tfun(t1,type_list), Some cons_list -> Some((t,t1) :: make_constrainsts (types_of_cons_params cons_list) (type_list) @ constraints_of_cons_params cons_list )
+                        | _, _, _ -> None)
+   | Return e -> (match gamma "__ret", get_constraints gamma e with
+                  | Some t1, Some (t2,c2) -> Some((t1,t2) :: c2)
+                  | _, _ -> None)
+   )
+
 type substitution = tident -> typ option
 let empty_subst : substitution = fun _ -> None
 let empty_context : context = fun _ -> None
 let update g x t = fun y -> if y = x then Some t else g y
 let single_subst x t : substitution = update empty_subst x t
+
                                         
 let rec apply_subst (s : substitution) (t : typ) : typ =
   match t with
@@ -86,6 +133,7 @@ let rec apply_subst (s : substitution) (t : typ) : typ =
   | Ttuple (t1, t2) -> Ttuple (apply_subst s t1, apply_subst s t2)
   | TArray t1 -> TArray (apply_subst s t1)
   | Tvar x -> (match s x with Some t -> t | None -> Tvar x)
+  | Tfun(t1,type_list) -> Tfun (apply_subst s t1, List.map (apply_subst s) type_list ) 
 
 let compose_subst s1 s2 = fun x ->
   match s2 x with
@@ -129,3 +177,11 @@ let infer_type (e : exp) =
              | Some s -> Some (apply_subst s t)
              | None -> None)
          | None -> None
+
+let infer_type_c (c : cmd) : bool =
+            match get_constraints_c empty_context c with
+            | Some c ->
+               (match unify c with
+                | Some s -> true
+                | None -> false)
+            | None -> false
